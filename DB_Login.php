17a878451c8580cde55226adb818ec3ca9737975
PK392 <pratiksuhas@gmail.com>
2015-09-22 16:08:25 -0700

    Create DB_Login.php

---
+++ DB_Login.php
@@ -0,0 +1,24 @@
+<?php 
+$servername="localhost";
+$username="root";
+$password=" ";
+$dbname="COMPANY";
+
+//Create a connection 
+$conn= new mysql($servername,$username,$password);
+$dconn= mysql_select_db($dbname,$conn) ;
+
+
+//check connection
+
+if ($conn->connect_error)
+{
+	die("Connection failed:" .$conn->connect_error);
+}
+echo "Connected Successfully";
+
+if ($dconn->connect_error)
+{
+	die("Couldnot Connect to dataabse: .$dconn->connect-error")
+}
+?>
