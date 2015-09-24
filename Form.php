a4a29da6f9ef336cc931fa513e61f48623ff8b48
PK392 <pratiksuhas@gmail.com>
2015-09-24 14:02:11 -0700

    Update Form.php

--- Form.php
+++ Form.php
@@ -28,19 +28,7 @@ echo'<td><input type=\"text\" name=\"Deptid_text\"></td><td><input type=\"text\"
 echo'</tr>';
 
 echo '</table>';
- $Didtext = mysql_real_escape_string($_POST['Deptid_text']);
- $Dnametext=mysql_real_escape_string($_POST['DeptName_text']);
- $Desctext=mysql_real_escape_string($_POST['Desc_text']);
- 
- $adddept="INSERT INTO Department('Dept_Id','Dname','Description') values('Didtext','Dnametext','Desctext')";
- $result = mysql_query($adddept);
-if($result)
-{
-    echo("<br>Input data is succeed");
-} else
-{
-    echo("<br>Input data failed");
-}
+
 echo'<input type="submit" value="Add_Department">';
 ?>
 </form>
