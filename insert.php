a7a08dd41be5d29f700228a0207cd53f6297ed75
PK392 <pratiksuhas@gmail.com>
2015-09-27 15:21:44 -0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -9,7 +9,9 @@
  echo $Didtext;
  echo $Dnametext;
  
- $adddept="INSERT INTO Department('Dept_Id','Dname','Description') values('Didtext','Dnametext','Desctext')";
+ $adddept="INSERT INTO Department('Dept_Id','Dname','Description') 
+
+values('".$Didtext."','".$Dnametext."','".$Desctext."')";
  $result = mysql_query($adddept);
 if($result)
 {
