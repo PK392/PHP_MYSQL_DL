047e8f032c549d8575b6d8d1b553b66c773aa632
PK392 <pratiksuhas@gmail.com>
2015-09-27 13:03:43 -0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,24 +1,20 @@
 <?php
+ 
  require_once('DB_Login.php');
- foreach($_POST as $key => $value)
-	{
-		if($key != "SEND") //Prevent the submit button's name and value from being inserted into the db
-		{
-		 $adddept = "INSERT INTO Department ($key) VALUES ('$value')";
-			$result=mysql_query($adddept) or die(mysql_error());
-			if($result)
+ 
+ $Didtext = mysql_real_escape_string($_POST['Dept_Id']);
+ $Dnametext=mysql_real_escape_string($_POST['Dname']);
+ $Desctext=mysql_real_escape_string($_POST['Description']);
+ 
+ $adddept="INSERT INTO Department('Dept_Id','Dname','Description') values('Didtext','Dnametext','Desctext')";
+ $result = mysql_query($adddept);
+if($result)
 {
     echo("<br>Input data is succeed");
-} 
-else
+} else
 {
     echo("<br>Input data failed");
 }
-			
-		}
-	}
- 
-
-
 
 ?>
+ 
