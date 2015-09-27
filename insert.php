ad0aa06969502c829ea22147cec3596838c27a65
PK392 <pratiksuhas@gmail.com>
2015-09-27 13:32:15 -0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -6,6 +6,9 @@
  $Dnametext=mysql_real_escape_string($_POST['Dname']);
  $Desctext=mysql_real_escape_string($_POST['Description']);
  
+ echo('$Didtext');
+ echo('Dnametext');
+ 
  $adddept="INSERT INTO Department('Dept_Id','Dname','Description') values('Didtext','Dnametext','Desctext')";
  $result = mysql_query($adddept);
 if($result)
