065e19c685de4a257bec674dd13155878a40805d
PK392 <pratiksuhas@gmail.com>
2015-09-28 14:09:57 -0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,10 +1,27 @@
 <?php
  
- require_once('DB_Login.php');
+$servername="localhost";
+$username="root";
+$password="Master@1234";
+$dbname="COMPANY";
+//Create a connection 
+$conn=mysql_connect($servername,$username,$password);
+$dconn= mysql_select_db($dbname,$conn) ;
+//check connection
+if ($conn->connect_error)
+{
+	die("Connection failed:" .$conn->connect_error);
+}
+echo "Connected Successfully";
+if ($dconn->connect_error)
+{
+	die("Couldnot Connect to database: .$dconn->connect-error");
+}
+echo"Connected to Database";
  
- $Didtext = mysqli_real_escape_string($_POST['Dept_Id']);
- $Dnametext= mysqli_real_escape_string($_POST['Dname']);
- $Desctext= mysqli_real_escape_string($_POST['Description']);
+ $Didtext = mysqli_real_escape_string($conn,$_POST['Dept_Id']);
+ $Dnametext= mysqli_real_escape_string($conn,$_POST['Dname']);
+ $Desctext= mysqli_real_escape_string($conn,$_POST['Description']);
  
  echo $Didtext;
  echo $Dnametext;
@@ -12,7 +29,7 @@
  $adddept="INSERT INTO Department(Dept_Id,Dname,Description) 
 
 values('$Didtext','$Dnametext','$Desctext')";
- $result = mysqli_query($adddept);
+ $result = mysql_query($adddept);
 if($result)
 {
     echo("<br>Input data is succeed");
