072dae6944552e23f77feea9fd1e31061044dc95
PK392 <pratiksuhas@gmail.com>
2015-09-28 13:36:55 -0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -2,7 +2,7 @@
  
  require_once('DB_Login.php');
  
- $Didtext = mysqli_real_escape_string($_POST['Dept_Id']);
+ $Didtext = (int)$_POST['Dept_Id'];
  $Dnametext= mysqli_real_escape_string($_POST['Dname']);
  $Desctext= mysqli_real_escape_string($_POST['Description']);
  
@@ -12,7 +12,7 @@
  $adddept="INSERT INTO Department(Dept_Id,Dname,Description) 
 
 values('$Didtext','$Dnametext','$Desctext')";
- $result = mysql_query($adddept);
+ $result = mysqli_query($adddept);
 if($result)
 {
     echo("<br>Input data is succeed");
