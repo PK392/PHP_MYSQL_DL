65a9342badd1d4eb6e01d3f3abeefc76de6dac14
PK392 <pratiksuhas@gmail.com>
2015-09-29 16:41:01 -0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -23,12 +23,13 @@ echo"Connected to Database";
  $Dnametext= mysql_real_escape_string($_POST['Dname']);
  $Desctext= mysql_real_escape_string($_POST['Description']);
  
- echo $Didtext;
- echo $Dnametext;
+ echo '.$Didtext';
+ echo '.$Dnametext';
  
  $adddept="INSERT INTO Department(Dept_Id,Dname,Description) 
 
 values($Didtext,'$Dnametext','$Desctext')";
+echo '.$addept';
  $result = mysql_query($adddept);
 if($result)
 {
